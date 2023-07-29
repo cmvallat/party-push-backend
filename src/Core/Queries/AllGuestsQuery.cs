b01d40ce541d5feda6af911a2dda5b5478c1c77e
Christian Vallat <christianvallat02@gmail.com>
2023-07-29 09:18:19 -0600

    returning full lists on add/delete, new all guest list

---
+++ src/Core/Queries/AllGuestsQuery.cs
@@ -0,0 +1,30 @@
+using Mediator;
+using Models;
+using DataLayer;
+using System.ComponentModel.DataAnnotations;
+
+namespace Core.Queries;
+
+public class AllGuestsQuery
+{
+    public class Query : IRequest<List<Guest>>
+    {
+        [Required]
+        public string Party_code { get; set; }
+    }
+
+    public class Handler : IRequestHandler<Query, List<Guest>>
+    {
+        private readonly IPartyService _dbService;
+
+        public Handler(IPartyService dbService)
+        {
+            _dbService = dbService ?? throw new ArgumentNullException(nameof(dbService));
+        }
+
+        public async ValueTask<List<Guest>> Handle(Query query, CancellationToken cancellationToken)
+        {
+            return await _dbService.GetAllGuestList(query.Party_code);
+        }
+    }
+}
--- src/DataLayer/IPartyService.cs
+++ src/DataLayer/IPartyService.cs
@@ -5,22 +5,29 @@ namespace DataLayer;
 public interface IPartyService
 {
     //Check-In API functions
+    //Commands
     Task<string> AddGuestFromHost(string guest_name, string party_code);
     Task<string> AddGuestFromCheckIn(Guest guest);
     Task<string> UpdateGuest(Guest guest);
     Task<string> CreateParty(Host host);
+    Task<string> DeleteGuest(string party_code, string guest_name);
+    Task<string> EndParty(string party_code);
+
+    //Queries
     Task<Guest> GetGuest(string party_code, string guest_name);
     Task<Host> GetHost(string party_code);
     Task<Host> GetHostFromCheckIn(string party_code, string phone_numner, string password);
-    Task<string> DeleteGuest(string party_code, string guest_name);
     Task<List<Guest>> GetGuestList(string party_code);
-    Task<string> EndParty(string party_code);
+    Task<List<Guest>> GetAllGuestList(string party_code);
     
     //Refreshment API functions
-    Task<List<Food>> GetCurrentFoodList(string party_code);
+    //Commands
     Task<string> AddFoodItem(string party_code, string item_name);
     Task<string> RemoveFoodItem(string party_code, string item_name);
     Task<string> ChangeFoodStatus(string party_code, string status, string item_name);
+
+    //Queries
+    Task<List<Food>> GetCurrentFoodList(string party_code);
 
     //Todo: add spotify API functions when implementing feature
 }
