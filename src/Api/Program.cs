d49ee37450c01f8a40cdc148e642c4835b173cee
Christian Vallat <christianvallat02@gmail.com>
2024-01-07 12:49:29 -0600

    restructured program.cs and startup.cs

--- src/Api/Program.cs
+++ src/Api/Program.cs
@@ -1,82 +1,20 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
-using System.Diagnostics;
-using System.Net;
-using DataLayer;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
 
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddSingleton<IDatabaseConnectionFactory, PartyConnectionFactory>();
-builder.Services.AddSingleton<IPartyService, PartyService>();
-builder.Services.AddMediator();
-
-builder.Services.AddControllers();
-builder.Services.AddHealthChecks();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-//JWT Authentication
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options => {
-    options.TokenValidationParameters = new TokenValidationParameters {
-        ValidateIssuer = true,
-            ValidateAudience = true,
-            ValidateLifetime = true,
-            ValidateIssuerSigningKey = true,
-            ValidIssuer = "https://localhost:5001/",
-            ValidAudience = "https://localhost:5001/",
-            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("6xGrJLpenwmJXCIAlLPbdfbgctrbgvrtgcrtdbgvgrdffvxrsvfdfrcvftryr65gr4sdrger"))
-    };
-});
-
-builder.Services.AddCors(options =>
+namespace Api
 {
-    options.AddPolicy("AllowSpecificOrigin",
-        builder => builder
-        .WithOrigins("http://localhost:3000",
-                    "http://54.208.105.152:3000",
-                    "https://livepartyhelper.com",
-                    "https://www.livepartyhelper.com",
-                    "https://api.twilio.com",
-                    "http://api.twilio.com"));
-});
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
 
-var app = builder.Build();
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
-app.UseHttpsRedirection();
-
-app.UseCors(options =>
-{
-    options.WithOrigins("http://localhost:3000",
-                    "http://54.208.105.152:3000",
-                    "https://livepartyhelper.com",
-                    "https://www.livepartyhelper.com",
-                    "https://api.twilio.com",
-                    "http://api.twilio.com")
-           .WithHeaders("X-Requested-With", "Content-Type", "Accept", "Authorization")
-           .WithMethods("GET", "POST", "PUT", "DELETE")
-           .WithExposedHeaders("content-disposition")
-           .WithExposedHeaders("content-length")
-           .WithExposedHeaders("content-encoding")
-           .WithExposedHeaders("content-type")
-           .WithExposedHeaders("x-filename")
-           .WithExposedHeaders("x-filesize")
-           .WithExposedHeaders("access-control-allow-origin");
-});
-
-app.UseAuthentication();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.MapHealthChecks("/healthcheck");
-
-app.Run();
-
-public partial class Program {  }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -3,8 +3,20 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics;
+using System.Net;
+using DataLayer;
+using Mediator;
+using Microsoft.OpenApi.Models;
+using System;
+using MediatR;
 
-namespace MyNamespace
+
+
+namespace Api
 {
     public class Startup
     {
@@ -17,21 +29,57 @@ namespace MyNamespace
 
         public void ConfigureServices(IServiceCollection services)
         {
+              // Register MediatR
+            services.AddMediator();
+
+            // Add other services
+            services.AddSingleton<IDatabaseConnectionFactory, PartyConnectionFactory>();
+            services.AddSingleton<IPartyService, PartyService>();
+
+            // Add controllers, health checks, and API explorer
+            services.AddControllers();
+            services.AddHealthChecks();
+            services.AddEndpointsApiExplorer();
+
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowSpecificOrigin",
                     builder => builder
-                    .WithOrigins(
-                        "http://localhost:3000",
-                        "http://54.208.105.152:3000",
-                        "https://livepartyhelper.com",
-                        "https://www.livepartyhelper.com",
-                        "https://api.twilio.com",
-                        "http://api.twilio.com")
-                    .AllowAnyHeader()
-                    .AllowAnyMethod());
+                        .WithOrigins(
+                            "http://localhost:3000",
+                            "http://54.208.105.152:3000",
+                            "https://livepartyhelper.com",
+                            "https://www.livepartyhelper.com",
+                            "https://api.twilio.com",
+                            "http://api.twilio.com")
+                        .AllowAnyHeader()
+                        .AllowAnyMethod());
+            });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = "https://localhost:5001/",
+                        ValidAudience = "https://localhost:5001/",
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("6xGrJLpenwmJXCIAlLPbdfbgctrbgvrtgcrtdbgvgrdffvxrsvfdfrcvftryr65gr4sdrger"))
+                    };
                 });
+
+            services.AddControllers();
+
+            // Swagger configuration
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Your API", Version = "v1" });
+            });
         }
+
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -40,7 +88,18 @@ namespace MyNamespace
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+            });
+
+            app.UseHttpsRedirection();
+
             app.UseCors("AllowSpecificOrigin");
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseRouting();
 
@@ -49,5 +108,6 @@ namespace MyNamespace
                 endpoints.MapControllers();
             });
         }
+
     }
 }
